69894a0632f5173e8ec49d80540c7004fc8442a7
Khongduytuan <103521567+Khongduytuan@users.noreply.github.com>
2024-05-30 13:55:58 +0700

    Hoàn thiện full các chức năng cuối cùng

---
+++ app/src/main/java/com/eagletech/myalarm/context/App.kt
@@ -0,0 +1,12 @@
+package com.eagletech.myalarm.context
+
+import android.app.Application
+import android.content.Context
+
+class App : Application() {
+    var cnt: Context? = null
+    override fun onCreate() {
+        super.onCreate()
+        cnt = this
+    }
+}
